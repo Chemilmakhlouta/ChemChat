b9889bcd5311ffb19c1bd8c300baaf468ffa4b83
Chemil Makhlouta <chemil.makhlouta@suncorp.com.au>
2018-07-24 23:19:11 +1000

    Implement clean MVP folder structure

---
+++ app/src/main/java/chemilmakhlouta/seekapp/application/injection/annotation/PerScreen.kt
@@ -0,0 +1,11 @@
+package chemilmakhlouta.seekapp.application.injection.annotation
+
+import javax.inject.Scope
+
+/**
+ * Created by Chemil Makhlouta on 24/7/18.
+ */
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerScreen
--- app/src/main/java/chemilmakhlouta/seekapp/presentation/MainActivity.java
+++ app/src/main/java/chemilmakhlouta/seekapp/presentation/MainActivity.java
@@ -1,7 +1,9 @@
-package chemilmakhlouta.seekapp;
+package chemilmakhlouta.seekapp.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import chemilmakhlouta.seekapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
